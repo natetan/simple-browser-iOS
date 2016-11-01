c4ef87e325a361ee751e54cca6c5029876cc89a1
Yulong Tan <yulongtan67@gmail.com>
2016-10-31 19:40:21 -0700

    Initial Commit

---
+++ Webs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Webs
+//
+//  Created by iGuest on 10/31/16.
+//  Copyright © 2016 yulongproductions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
